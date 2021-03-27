ce94bed9500104fc24a2486531345535402210a7
Ian Fan <ian.fan13@gmail.com>
2021-03-27 18:36:11 -0400

    13.4.1

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -7,32 +7,13 @@ let map = L.map("mapid", {
     zoom: 4
 });
 
-/*
+
 L.circleMarker([34.0522, -118.2437], {
     radius: 300,
     color: "black",
     fillColor: "#ffffa1"
 
 }).addTo(map);
-*/
-
-// An array containing each city's location, state, and population.
-let cityData=cities;
-
-// Loop through the cities array and create one marker for each city.
-cities.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location,
-        {
-         radius: city.population/100000,
-         color: "orange",
-         fillColor: "orange",
-         fillOpacity: 0.25,
-         weight: 4
-        })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-    .addTo(map);
-});
 
 // create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
