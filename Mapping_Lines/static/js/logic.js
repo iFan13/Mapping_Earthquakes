6f2446a2c929993f105e2a006ade005aee300fec
Ian Fan <ian.fan13@gmail.com>
2021-03-27 19:32:20 -0400

    13.4.3.1

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -3,27 +3,29 @@ console.log("working");
 
 // Create the map object with a center and zoom level.
 let map = L.map("mapid", {
-    center: [36.1733,-120.1794],
-    zoom: 7
+    center: [37.6213, -122.3790],
+    zoom: 5
 });
 
 // Coordinates for each point to be used in the line.
 let line = [
     [33.9416, -118.4085],
-    [37.6213, -122.3790]
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
   ];
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-    color: "red"
-  }).addTo(map);
-
+    color: "yellow"
+ }).addTo(map);
 
 // create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     //id: " <chosen from https://docs.mapbox.com/api/maps/styles/> ",
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: "satellite-streets-v11",
     maxZoom: 18,
     accessToken: API_KEY
 });
