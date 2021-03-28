a1e505a790f3f98d0afe690d1315c27b6d7278db
Ian Fan <ian.fan13@gmail.com>
2021-03-27 20:33:47 -0400

    13.5.4

--- Mapping_GeoJSON_Points/static/js/logic.js
+++ Mapping_GeoJSON_Points/static/js/logic.js
@@ -1,9 +1,3 @@
-// Create the map object with a center and zoom level.
-let map = L.map("mapid", {
-    center: [30, 30],
-    zoom: 2
-});
-
 // create the tile layer that will be the background of our map.
 //let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     //id: " <chosen from https://docs.mapbox.com/api/maps/styles/> ",
@@ -14,8 +8,25 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z
     accessToken: API_KEY
 });
 
-// add tile layer to the map.
-streets.addTo(map);
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: "dark-v10",
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+
+let baseMaps = {Street: streets, Dark: dark};
+
+// Create the map object with a center and zoom level.
+let map = L.map("mapid", {
+    center: [30, 30],
+    zoom: 2,
+    layers:[streets]
+});
+
+
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
 
 // Accessing the airport GeoJSON URL
 let airportData = "https://raw.githubusercontent.com/iFan13/Mapping_Earthquakes/main/majorAirports.json";
